e369289292aee8e56a700173c454d15e9289acc6
MIRIAMDART <dartehmimi@gmsil.com>
2018-07-16 04:14:56 +0000

    new

---
+++ static/home.js
@@ -0,0 +1,9 @@
+window.toggleLeft = function() {
+    var element = document.getElementById("offcanvas-left");
+    element.classList.toggle("hide");
+}
+window.toggleRight = function() {
+    var element = document.getElementById("offcanvas-right");
+    element.classList.toggle("hide");
+}
+
